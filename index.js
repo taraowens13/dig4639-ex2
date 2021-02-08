179b074995d6ec43c028a8eb72e6f2b735db6f90
taraowens13 <74627961+taraowens13@users.noreply.github.com>
2021-02-07 19:10:16 -0500

    closes #1

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let quotes = [
-    '"Always remember that you are absolutely unique. Just like everyone else." - Margaret Mead',
+    '"Always remember that you are absolutely unique. Just like everyone else." -Margaret Mead',
     '"I have learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel." -Maya Angelou',
     '"The only true wisdom is knowing that you know nothing" -Socrates',
+    '"We must not allow other peoples limited perceptions to define us." -Virginia Satir',
 ]
-let index = (Math.floor(Math.random() * 3))
+let index = (Math.floor(Math.random() * 4))
 console.log(quotes[index])
