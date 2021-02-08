06ba088730baf542ba2473a9a960a73188fe9ebd
taraowens13 <74627961+taraowens13@users.noreply.github.com>
2021-02-07 19:06:03 -0500

    Create quote app

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # dig4639-ex2
+
+"Always remember that you are absolutely unique. Just like everyone else."
+-Margaret Mead
---
+++ index.js
@@ -0,0 +1,7 @@
+let quotes = [
+    '"Always remember that you are absolutely unique. Just like everyone else." - Margaret Mead',
+    '"I have learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel." -Maya Angelou',
+    '"The only true wisdom is knowing that you know nothing" -Socrates',
+]
+let index = (Math.floor(Math.random() * 3))
+console.log(quotes[index])
